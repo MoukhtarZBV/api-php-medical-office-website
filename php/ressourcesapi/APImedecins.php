3c6a63a28ab4f9d05e8cec2eb0798591501fa78c
Pema <rasoanaivo.pema@gmail.com>
2024-03-14 09:27:23 +0100

    Error codes changed

--- php/ressourcesapi/APImedecins.php
+++ php/ressourcesapi/APImedecins.php
@@ -6,13 +6,15 @@ require("utilitairesAPI.php");
 $pdo = new PDO("mysql:host=mysql-medical-office.alwaysdata.net;dbname=medical-office_ressources", '350740', '$iutinfo');
 $http_method = $_SERVER['REQUEST_METHOD'];
 switch ($http_method) {
+
     case "GET":
+
         if (isset($_GET["id"])) {
             $id = $_GET["id"];
             if (!empty($medecin = getMedecinById($pdo, $id))) {
                 fournirReponse("Succes", 200, "Medecin d'ID : ".$id." récuperé", $medecin);
             } else {
-                fournirReponse("Erreur", 400, "Medecin d'ID : ".$id." inexistant");
+                fournirReponse("Erreur", 404, "Medecin d'ID : ".$id." inexistant");
             }
         } else if (!isset($_GET["civilite"]) &&
             !isset($_GET["nom"]) &&
@@ -20,7 +22,7 @@ switch ($http_method) {
                 if (!empty($medecin = getMedecins($pdo, null, null, null))) {
                     fournirReponse("Succes", 200, "Tous les médecins récuperés", $medecin);
                 } else {
-                    fournirReponse("Erreur", 400, "Aucun médecin récuperé");
+                    fournirReponse("Erreur", 404, "Aucun médecin récuperé");
                 }
         } else {
             $civilite = isset($_GET["civilite"]) ? $_GET["civilite"] : null;
@@ -28,11 +30,16 @@ switch ($http_method) {
             $prenom = isset($_GET["prenom"]) ? $_GET["prenom"] : null;
             if ($medecinsFiltres = getMedecins($pdo, $civilite, $nom, $prenom)) {
                 fournirReponse("Succes", 200, "Consultations filtrées récuperées", $medecinsFiltres);
+            } else {
+                fournirReponse("Succes", 404, "Aucun médecin trouvé correspondant à vos filtres", $medecinsFiltres);
             }
         }
         break;
+
     case "POST":
+
         $jwt = get_bearer_token();
+
         if ($jwt && jetonValide($jwt)) {
             $contenuFichier = file_get_contents('php://input');
             $arguments = json_decode($contenuFichier, true); 
@@ -43,13 +50,13 @@ switch ($http_method) {
                         $medecin = getMedecinById($pdo, $id);
                         fournirReponse("Succes", 200, "Médecin crée", $medecin);
                     } else {
-                        fournirReponse("Erreur", 400, "Erreur lors de la création du médecin");
+                        fournirReponse("Erreur", 400, "Une erreur est survenue, le médecin n'a pas pu être ajouté");
                     }
             } else {
-                fournirReponse("Erreur", 400, "Création médecin impossible, tous les champs ne sont pas renseignés");
+                fournirReponse("Erreur", 422, "Création du médecin impossible, tous les champs ne sont pas renseignés");
             }
         } else {
-            fournirReponse("Erreur", 400, "Jeton invalide");
+            fournirReponse("Erreur", 401, "Jeton invalide");
         }
         break;
     case "DELETE":
@@ -62,10 +69,10 @@ switch ($http_method) {
                     fournirReponse("Erreur", 400, "Aucun médecin supprimé");
                 }
             } else {
-                fournirReponse("Erreur", 400, "Paramètre ID non spécifié");
+                fournirReponse("Erreur", 422, "Paramètre ID non spécifié");
             }
         } else {
-            fournirReponse("Erreur", 400, "Jeton invalide");
+            fournirReponse("Erreur", 401, "Jeton invalide");
         }
         break;
     case "PATCH":
@@ -88,12 +95,12 @@ switch ($http_method) {
                     fournirReponse("Erreur", 400, "Médecin d'ID : ".$id." inchangé");
                 }
             } else if (empty($_GET["id"])) {
-                fournirReponse("Erreur", 400, "Paramètre ID non spécifié");
+                fournirReponse("Erreur", 422, "Paramètre ID non spécifié");
             } else {
-                fournirReponse("Erreur", 400, "Au moins un paramètre doit être saisi");
+                fournirReponse("Erreur", 422, "Au moins un paramètre doit être saisi");
             }
         } else {
-            fournirReponse("Erreur", 400, "Jeton invalide");
+            fournirReponse("Erreur", 401, "Jeton invalide");
         }
         break;
     case "PUT":
@@ -116,14 +123,14 @@ switch ($http_method) {
                     fournirReponse("Erreur", 400, "Médecin d'ID : ".$id." inchangé");
                 }
             } else if (empty($_GET["id"])) {
-                fournirReponse("Erreur", 400, "Paramètre ID non spécifié");
+                fournirReponse("Erreur", 422, "Paramètre ID non spécifié");
             } else {
-                fournirReponse("Erreur", 400, "Tous les paramètres doivent être saisis");
+                fournirReponse("Erreur", 422, "Tous les paramètres doivent être saisis");
             }
         } else {
-            fournirReponse("Erreur", 400, "Jeton invalide");
+            fournirReponse("Erreur", 401, "Jeton invalide");
         }
         break;
 }
-
+?>
 
